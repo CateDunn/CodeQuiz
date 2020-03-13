148a8eb2c2bce4b3bd14db76ab19153d580e08d5
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-03-13 14:17:44 -0400

    adding start button

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('testing');
+
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
+
+startButton.addEventListener('click', startQuiz)
+
+
+function startQuiz() {
+  startButton.classList.add('hide')
+  questionContainerElement.classList.remove('hide')
+}
